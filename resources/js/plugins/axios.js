fa20c9b562572c0139302189e47cbdec8f0f5e25
havaweb <nguyenngocha18062002@gmail.com>
2023-06-10 23:27:35 +0700

    first commit

---
+++ resources/js/plugins/axios.js
@@ -0,0 +1,10 @@
+import axios from '@axios'
+
+const axiosIns = axios.create({
+  // You can add your headers here
+  // ================================
+  baseURL: 'http://127.0.0.1:8000/',
+  headers: { 'Accept': 'application/json' },
+})
+
+export default axiosIns
